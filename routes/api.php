1413fb876d9460ed4d4c521d7b6cc1191432fdba
mzcs84Jj <matheus.zcs@gmail.com>
2021-10-22 20:21:56 -0300

    CRUD Cadastro de Produtos

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('produtos')->group(function(){
+    Route::get('/',[ProdutosController::class, 'index']);
+    Route::get('/{id}',[ProdutosController::class, 'show']);
+    Route::post('/',[ProdutosController::class, 'store']);
+    Route::put('/{id}',[ProdutosController::class, 'update']);
+    Route::delete('/{id}',[ProdutosController::class, 'destroy']);
 });
