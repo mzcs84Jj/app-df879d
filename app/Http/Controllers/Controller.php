7c1f0b9a04773b07f042cc4716cd99e2adb7590f
mzcs84Jj <matheus.zcs@gmail.com>
2021-10-23 01:09:32 -0300

    Documentação com Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Server(url="http://127.0.0.1:8000/api"),
+ * @OA\Info(title="Appmax API Documentation", version="0.0.1")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/Http/Controllers/MovimentacaoController.php
+++ app/Http/Controllers/MovimentacaoController.php
@@ -8,6 +8,17 @@ use Illuminate\Http\Request;
 
 class MovimentacaoController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      tags={"/movimentacao"},
+     *      summary="Retorna o histórico das movimentações",
+     *      description="Retorna um objeto com o histórico das movimentações",
+     *      path="/movimentacao",
+     *      @OA\Response(
+     *          response="200", description="Histórico de movimentações"
+     *      )
+     * )
+     */    
     public function index()
     {
         $result = Movimentacao::select('*')
@@ -16,6 +27,29 @@ class MovimentacaoController extends Controller
         return response()->json($result);
     }    
 
+    /**
+     * @OA\Get(
+     *      tags={"/movimentacao"},
+     *      summary="Retorna uma movimentação especifica",
+     *      description="Retorna os dados de uma movimentação especifica",
+     *      path="/movimentacao/{id}",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          description="Id da movimentação",
+     *          required=false,
+     *          @OA\Schema(
+     *              type="bigint"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="200", description="Dados da movimentação"
+     *      ),
+     *      @OA\Response(
+     *          response="401", description="Movimentação não encontrada"
+     *      )
+     * )
+     */
     public function show($id)
     {
         $result = Movimentacao::find($id);
@@ -30,9 +64,41 @@ class MovimentacaoController extends Controller
         return response()->json($result);
     }    
 
+    /**
+     * @OA\Post(
+     *      tags={"/movimentacao"},
+     *      summary="Insere uma movimentação",
+     *      description="Insere uma movimentação",
+     *      path="/movimentacao",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="id", type="integer"),
+     *              @OA\Property(property="sku", type="string"),
+     *              @OA\Property(property="qtd", type="integer"), 
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response="200", description="Movimentação inserida com sucesso"
+     *      ),
+     *      @OA\Response(
+     *          response="401", description="Produto não encontrado"
+     *      )
+     * )
+     */      
     public function store(Request $request)
     {
         try {
+            $produto = Produtos::find($request->sku);
+
+            if (!$produto) {
+                return response()->json([
+                    'error' => true,
+                    'message' => 'Produto não encontrado.'
+                ], 401);    
+            }
+
             $mov = new Movimentacao();
             $mov->fill($request->all());
 
@@ -51,6 +117,29 @@ class MovimentacaoController extends Controller
         }
     }    
 
+   /**
+     * @OA\Put(
+     *      tags={"/movimentacao"},
+     *      summary="Atualiza uma movimentação",
+     *      description="Atualiza uma movimentação",
+     *      path="/movimentacao/{id}",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="id", type="integer"),
+     *              @OA\Property(property="sku", type="string"),
+     *              @OA\Property(property="qtd", type="integer"), 
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response="200", description="Movimentação atualizada com sucesso"
+     *      ),
+     *      @OA\Response(
+     *          response="401", description="Movimentação ou Produto não encontrados"
+     *      ),
+     * )
+     */       
     public function update(Request $request, $id)
     {
         try {
@@ -63,6 +152,15 @@ class MovimentacaoController extends Controller
                 ], 401);
             }
     
+            $produto = Produtos::find($request->sku);
+
+            if (!$produto) {
+                return response()->json([
+                    'error' => true,
+                    'message' => 'Produto não encontrado.'
+                ], 401);    
+            }
+
             $mov->fill($request->all());
             $mov->save();
 
@@ -79,6 +177,29 @@ class MovimentacaoController extends Controller
         }
     }    
 
+     /**
+     * @OA\Delete(
+     *      tags={"/movimentacao"},
+     *      summary="Exclui uma movimentação",
+     *      description="Exclui uma movimentação",
+     *      path="/movimentacao/{id}",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          description="Id da movimentação",
+     *          required=false,
+     *          @OA\Schema(
+     *              type="bigint"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="200", description="Movimentação excluida com sucesso"
+     *      ),
+     *      @OA\Response(
+     *          response="401", description="Movimentação não encontrada"
+     *      ),
+     * )
+     */  
     public function destroy($id)
     {
         try{
@@ -105,6 +226,29 @@ class MovimentacaoController extends Controller
         }                   
     }    
 
+    /**
+     * @OA\Get(
+     *      tags={"/movimentacao"},
+     *      summary="Retorna as movimentações de um produto",
+     *      description="Retorna as movimentações de um produto",
+     *      path="/movimentacao/sku/{sku}",
+     *      @OA\Parameter(
+     *          name="sku",
+     *          in="path",
+     *          description="SKU",
+     *          required=false,
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="200", description="Todas movimentações de um produto"
+     *      ),
+     *      @OA\Response(
+     *          response="401", description="Produto não encontrado"
+     *      )
+     * )
+     */         
     public function getMovimentacaoSku($sku)
     {
         $produto = Produtos::find($sku);
